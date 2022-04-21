f15efa25856aabee8440b69ff08f880100d57cec
asold2 <304147@via.dk>
2022-04-21 15:06:05 +0200

    placed TestController to package

--- src/main/java/SEP4Data/air4you/controllers/TestController.java
+++ src/main/java/SEP4Data/air4you/controllers/TestController.java
@@ -1,4 +1,4 @@
-package SEP4Data.air4you;
+package SEP4Data.air4you.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
