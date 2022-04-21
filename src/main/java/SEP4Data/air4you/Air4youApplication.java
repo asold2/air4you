23b452755736efebe3b6cb4aa0a490b7c10a097f
asold2 <304147@via.dk>
2022-04-21 15:23:38 +0200

    added /error label

--- src/main/java/SEP4Data/air4you/Air4youApplication.java
+++ src/main/java/SEP4Data/air4you/Air4youApplication.java
@@ -2,8 +2,10 @@ package SEP4Data.air4you;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Air4youApplication {
 
 	public static void main(String[] args) {
--- src/main/java/SEP4Data/air4you/controllers/TestController.java
+++ src/main/java/SEP4Data/air4you/controllers/TestController.java
@@ -1,15 +1,24 @@
 package SEP4Data.air4you.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/error")
+    @GetMapping("/test")
     String test(){
         return "Hi, my name is, what? My name is, who?\n" +
                 "My name is, chka-chka, Slim Shady";
+    }
+    @RequestMapping("/error")
+    @ResponseBody
+    String error(HttpServletRequest request) {
+        return "<h1>Error occurred</h1>";
     }
 
 }
