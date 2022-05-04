070753a3acc205cd3fb128de505dd2b1da1ecdc8
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-05-04 09:41:57 +0200

    Adding service-account-file.json for connecting with firebase firestore SDK

--- src/main/java/SEP4Data/air4you/TestController.java
+++ src/main/java/SEP4Data/air4you/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+ // s
     @GetMapping("/")
     String test(){
         return "Hi Data Warehouse group from SEP4 group 1!";
