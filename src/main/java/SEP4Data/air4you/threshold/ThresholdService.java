3a30f2f82dda71fb89de349f3538c602ec5e0133
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-05-23 15:49:22 +0200

    Revert Day class and create new HTTP method

--- src/main/java/SEP4Data/air4you/threshold/ThresholdService.java
+++ src/main/java/SEP4Data/air4you/threshold/ThresholdService.java
@@ -1,14 +1,17 @@
 package SEP4Data.air4you.threshold;
 
 import SEP4Data.air4you.tempThreshold.ISendTempThresholdToGateway;
+import SEP4Data.air4you.tempThreshold.TemperatureThreshold;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ThresholdService implements ISendTempThresholdToGateway {
 
 
+
+
     @Override
-    public void sendTempThresholdToGateway(Threshold temperatureThreshold) {
+    public void sendTempThresholdToGateway(TemperatureThreshold temperatureThreshold) {
 
     }
 }
