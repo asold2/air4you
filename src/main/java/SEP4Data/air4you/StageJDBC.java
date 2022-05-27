9ab3dcab59d40487951c8de5ecb1ce8a1c35f769
asold2 <304147@via.dk>
2022-05-27 16:03:34 +0200

    Soldan** null jdbc connection and no more new connection to db

--- src/main/java/SEP4Data/air4you/StageJDBC.java
+++ src/main/java/SEP4Data/air4you/StageJDBC.java
@@ -10,15 +10,15 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan
 public class StageJDBC {
-    @Bean
-    public DataSource stageDataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.postgres.jdbc.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
-
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource stageDataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+////        dataSource.setDriverClassName("com.postgres.jdbc.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//
+//        return dataSource;
+//    }
 
 }
