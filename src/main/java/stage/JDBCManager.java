72d828ccee2185e681cb232fceadef8e307ab266
asold2 <304147@via.dk>
2022-05-27 15:47:14 +0200

    Soldan** outcomented the extract in stage

--- src/main/java/stage/JDBCManager.java
+++ src/main/java/stage/JDBCManager.java
@@ -1,11 +1,8 @@
 package stage;
 
 import SEP4Data.air4you.StageJDBC;
-import org.hibernate.Session;
-import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.persistence.EntityManager;
 
 public class JDBCManager extends JdbcTemplate {
     private static JDBCManager instance;
