95f3bfe3838bf393e98755ff97f94af2520ef1c7
kyratolnai11 <266375@viauc.dk>
2022-05-31 09:33:05 +0200

    removed redundemt

--- src/main/java/SEP4Data/air4you/threshold/ThresholdRepository.java
+++
@@ -1,6 +0,0 @@
-package SEP4Data.air4you.threshold;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThresholdRepository extends JpaRepository<Threshold, Integer> {
-}
